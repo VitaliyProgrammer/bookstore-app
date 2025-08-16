8f323e8afe7d16cc90e0363e90deb8c49d5db53f
Vitaliy <vitaliyufc@gmail.com>
2025-08-16 11:17:02 +0300

    Fixed bookstore-app

--- src/main/java/com/example/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/bookstore/service/impl/BookServiceImpl.java
@@ -2,11 +2,12 @@ package com.example.bookstore.service.impl;
 
 import com.example.bookstore.model.Book;
 import com.example.bookstore.repository.BookRepository;
+import com.example.bookstore.service.BookService;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BookServiceImpl implements BookRepository {
+public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
 
     public BookServiceImpl(BookRepository bookRepository) {
