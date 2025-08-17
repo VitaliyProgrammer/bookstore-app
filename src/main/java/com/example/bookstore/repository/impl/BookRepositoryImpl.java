53d547ac3784933ea715498a02dd110f6ce0ec7a
Vitaliy <vitaliyufc@gmail.com>
2025-08-17 15:05:23 +0300

    Build for test-ci-fail-3

--- src/main/java/com/example/bookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/example/bookstore/repository/impl/BookRepositoryImpl.java
@@ -1,7 +1,6 @@
 package com.example.bookstore.repository.impl;
 
 
-
 import com.example.bookstore.model.Book;
 import com.example.bookstore.repository.BookRepository;
 import java.util.ArrayList;
@@ -21,6 +20,6 @@ public class BookRepositoryImpl implements BookRepository {
 
     @Override
     public List<Book> findAll() {
-                          return new ArrayList<>(books);
+        return new ArrayList<>(books);
     }
 }
--- src/test/java/com/example/bookstore/IndentationTest.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookstore;
-
-public class IndentationTest     {
-    public void test()     {
-        if (true ) {
-                  System.out.println("Hello");
-        }}
-}
