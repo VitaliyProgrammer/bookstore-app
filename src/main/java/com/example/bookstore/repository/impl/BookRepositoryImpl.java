e0b9ed6b1963dfa3cc68dd0decd205df099e3496
Vitaliy <vitaliyufc@gmail.com>
2025-08-18 20:48:36 +0300

    Fixed full-implementations: BookRepositoryImpl + BookServiceImpl + Checkstyle

--- src/main/java/com/example/bookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/example/bookstore/repository/impl/BookRepositoryImpl.java
@@ -2,24 +2,29 @@ package com.example.bookstore.repository.impl;
 
 import com.example.bookstore.model.Book;
 import com.example.bookstore.repository.BookRepository;
-import java.util.ArrayList;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.util.List;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
 
-    private final List<Book> books = new ArrayList<>();
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
+    @Transactional
     public Book save(Book book) {
-        books.add(book);
+        entityManager.persist(book);
         return book;
     }
 
     @Override
     public List<Book> findAll() {
-
-        return new ArrayList<>(books);
+        return entityManager
+                .createQuery("FROM Book", Book.class)
+                .getResultList();
     }
 }
